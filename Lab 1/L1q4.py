58121002e1dcfff583074c30cdf8eedea84b0d9f
Soumyadip Maity <22053029@kiit.ac.in>
2025-01-27 00:03:28 +0530

    Add script to print all prime numbers between 2 and 1000

--- Lab 1/L1q4.py
+++ Lab 1/L1q4.py
@@ -1,7 +1,8 @@
-# sum of all element in the array
-
-arr = [1, 2, 3, 4, 5]
-total = 0
-for num in arr:
-    total += num
-print("Sum of elements:", total)
+for num in range(2, 1001):
+    is_prime = True
+    for i in range(2, int(num**0.5) + 1):  
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
