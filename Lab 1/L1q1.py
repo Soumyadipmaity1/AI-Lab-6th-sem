2f04a67478bba4806913a79f7bb8d642f6d7d86f
Soumyadip Maity <22053029@kiit.ac.in>
2025-01-26 21:58:03 +0530

    Add number classification script

---
+++ Lab 1/L1q1.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a Number"))
+if number == 0:
+    print ("The number is 0")
+elif number < 0 :
+    print ("The number is negetive ")
+else :
+    print ("The number is positve")
